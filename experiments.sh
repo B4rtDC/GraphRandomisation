5bef715bbbdc60999260e968a7eda0e394db330d
Bart De Clerck <declerck.bart@pm.me>
2024-08-08 12:07:39 +0200

    forgot to add bipartite experiments

--- experiments.sh
+++ experiments.sh
@@ -5,3 +5,6 @@ julia --project=. -e 'include("./experiments/UndirectedExperiments.jl")'
 
 # Run the directed experiments (Chesapeake Bay foodweb)
 julia --project=. -e 'include("./experiments/DirectedExperiments.jl")'
+
+# Run the bipartite experiments (Crime & escort networks)
+julia --project=. -e 'include("./experiments/BipartiteExperiments.jl")'
